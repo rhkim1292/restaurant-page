b44533ac366cd2fdabe8a755fbfa1b7975866354
Randy Kim <rhkim1292@gmail.com>
2022-08-23 01:50:58 -0700

    Implement and style the menu tab

--- src/index.js
+++ src/index.js
@@ -4,81 +4,81 @@ import menuLoad from "./menu-load.js";
 import contactLoad from "./contact-load.js";
 
 const loadIndexLayout = (() => {
-    const _header = document.createElement("header");
-    const _nav = document.createElement("nav");
-    const _unorderedList = document.createElement("ul");
-    const _listItemHome = document.createElement("li");
-    const _homeBtn = document.createElement("div");
-    _homeBtn.id = "homeBtn";
-    _homeBtn.textContent = "Home";
-    _listItemHome.appendChild(_homeBtn);
-    const _listItemMenu = document.createElement("li");
-    const _menuBtn = document.createElement("div");
-    _menuBtn.id = "menuBtn";
-    _menuBtn.textContent = "Menu";
-    _listItemMenu.appendChild(_menuBtn);
-    const _listItemContact = document.createElement("li");
-    const _contactBtn = document.createElement("div");
-    _contactBtn.id = "contactBtn";
-    _contactBtn.textContent = "Contact";
-    _listItemContact.appendChild(_contactBtn);
-    _unorderedList.append(
-        _listItemHome,
-        _listItemMenu,
-        _listItemContact
-    );
-    _nav.appendChild(_unorderedList);
-    _header.appendChild(_nav);
-    const _contentElement = document.createElement("div");
-    _contentElement.id = "content";
-    homeLoad(_contentElement);
-    document.body.append(
-        _header,
-        _contentElement
-    );
+	const _header = document.createElement("header");
+	const _nav = document.createElement("nav");
+	const _unorderedList = document.createElement("ul");
+	const _listItemHome = document.createElement("li");
+	const _homeBtn = document.createElement("div");
+	_homeBtn.id = "homeBtn";
+	_homeBtn.textContent = "Home";
+	_listItemHome.appendChild(_homeBtn);
+	const _listItemMenu = document.createElement("li");
+	const _menuBtn = document.createElement("div");
+	_menuBtn.id = "menuBtn";
+	_menuBtn.textContent = "Menu";
+	_listItemMenu.appendChild(_menuBtn);
+	const _listItemContact = document.createElement("li");
+	const _contactBtn = document.createElement("div");
+	_contactBtn.id = "contactBtn";
+	_contactBtn.textContent = "Contact";
+	_listItemContact.appendChild(_contactBtn);
+	_unorderedList.append(_listItemHome, _listItemMenu, _listItemContact);
+	_nav.appendChild(_unorderedList);
+	_header.appendChild(_nav);
 
-    function getHomeBtn() {
-        return _homeBtn;
-    }
+	const _contentElement = document.createElement("div");
+	_contentElement.id = "content";
+	homeLoad(_contentElement);
 
-    function getMenuBtn() {
-        return _menuBtn;
-    }
+	const _footer = document.createElement("footer");
+	_footer.innerHTML =
+		"Home restaurant image by&nbsp;<a href='https://unsplash.com/@shawnanggg'>" +
+		"@shawnangg</a>&nbsp;on&nbsp;<a href='https://unsplash.com'>&nbsp;" +
+		"Unsplash.com</a>";
+	document.body.append(_header, _contentElement, _footer);
 
-    function getContactBtn() {
-        return _contactBtn;
-    }
+	function getHomeBtn() {
+		return _homeBtn;
+	}
 
-    function getContentElement() {
-        return _contentElement;
-    }
+	function getMenuBtn() {
+		return _menuBtn;
+	}
 
-    return {
-        getHomeBtn,
-        getMenuBtn,
-        getContactBtn,
-        getContentElement,
-    }
+	function getContactBtn() {
+		return _contactBtn;
+	}
+
+	function getContentElement() {
+		return _contentElement;
+	}
+
+	return {
+		getHomeBtn,
+		getMenuBtn,
+		getContactBtn,
+		getContentElement,
+	};
 })();
 
 const eventHandler = (() => {
-    const _homeBtn = loadIndexLayout.getHomeBtn();
-    const _menuBtn = loadIndexLayout.getMenuBtn();
-    const _contactBtn = loadIndexLayout.getContactBtn();
-    const _contentElement = loadIndexLayout.getContentElement();
+	const _homeBtn = loadIndexLayout.getHomeBtn();
+	const _menuBtn = loadIndexLayout.getMenuBtn();
+	const _contactBtn = loadIndexLayout.getContactBtn();
+	const _contentElement = loadIndexLayout.getContentElement();
 
-    _homeBtn.addEventListener("click", (e) => {
-        _contentElement.innerHTML = "";
-        homeLoad(_contentElement);
-    });
+	_homeBtn.addEventListener("click", (e) => {
+		_contentElement.innerHTML = "";
+		homeLoad(_contentElement);
+	});
 
-    _menuBtn.addEventListener("click", (e) => {
-        _contentElement.innerHTML = "";
-        menuLoad(_contentElement);
-    });
+	_menuBtn.addEventListener("click", (e) => {
+		_contentElement.innerHTML = "";
+		menuLoad(_contentElement);
+	});
 
-    _contactBtn.addEventListener("click", (e) => {
-        _contentElement.innerHTML = "";
-        contactLoad(_contentElement);
-    })
+	_contactBtn.addEventListener("click", (e) => {
+		_contentElement.innerHTML = "";
+		contactLoad(_contentElement);
+	});
 })();
--- src/menu-load.js
+++ src/menu-load.js
@@ -1,6 +1,32 @@
+import DiscoverImg from "./discover.jpg";
+
 export default function loadPage(parent) {
 	const _contentHeadline = document.createElement("h1");
 	_contentHeadline.textContent = "Menu";
+
+	const _entreeSectionHeader = document.createElement("h2");
+	_entreeSectionHeader.textContent = "Entrees";
+
+	const _entreeItem = document.createElement("div");
+	const _entreeItemTitle = document.createElement("h3");
+	const _entreeItemDescription = document.createElement("p");
+	const _entreeItemPrice = document.createElement("p");
+	const _entreeItemImg = new Image();
+	_entreeItem.id = "entreeItem1";
+	_entreeItemTitle.textContent = "Discover Food";
+	_entreeItemDescription.textContent =
+		"Much like how your favorite music app recommends new music for you " +
+		"to listen to, this entree item chooses a dish for you based on food " +
+		"you've ordered with us before!";
+	_entreeItemPrice.textContent = "$9";
+	_entreeItemImg.src = DiscoverImg;
+
+	_entreeItem.append(
+		_entreeItemTitle,
+		_entreeItemDescription,
+		_entreeItemPrice,
+		_entreeItemImg
+	);
 
 	// const _contentRestaurantImg = new Image();
 	// _contentRestaurantImg.classList.add("main-restaurant-img");
@@ -10,7 +36,5 @@ export default function loadPage(parent) {
 	// _contentDescription.textContent =
 	// 	"Our restaurant has the loveliest interior design and the finest cuisine one can imagine!";
 
-	parent.append(
-        _contentHeadline
-    );
+	parent.append(_contentHeadline, _entreeSectionHeader, _entreeItem);
 }
